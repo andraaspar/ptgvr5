bb5fd8ba40504553f218099a4c637cc0ce8d7159
andraaspar <andraaspar@gmail.com>
2019-06-24 08:14:38 +0200

    Updates

---
+++ precache-manifest.5046198787ade994055bbff5b64f3577.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867b2cc154f2b88e61b5f5d0f4f0a286",
+    "url": "/ptgvr5/index.html"
+  },
+  {
+    "revision": "a919d36ffc1a23b4034a",
+    "url": "/ptgvr5/static/css/2.7b7ba2a5.chunk.css"
+  },
+  {
+    "revision": "f8fe3f6805666d7c7d34",
+    "url": "/ptgvr5/static/css/main.24f68131.chunk.css"
+  },
+  {
+    "revision": "a919d36ffc1a23b4034a",
+    "url": "/ptgvr5/static/js/2.476b5ef5.chunk.js"
+  },
+  {
+    "revision": "f8fe3f6805666d7c7d34",
+    "url": "/ptgvr5/static/js/main.e09b6299.chunk.js"
+  },
+  {
+    "revision": "d3ba83cac0b8edd5268c",
+    "url": "/ptgvr5/static/js/runtime~main.7145adc6.js"
+  },
+  {
+    "revision": "82408b66052f4d1c4ebe7376ad490c8b",
+    "url": "/ptgvr5/static/media/wi-cloud.82408b66.svg"
+  },
+  {
+    "revision": "2d0410c52e9fe2b3e0fb905f129ad19a",
+    "url": "/ptgvr5/static/media/wi-cloudy.2d0410c5.svg"
+  },
+  {
+    "revision": "3237f7da426fbedf7faa6782f8c4c455",
+    "url": "/ptgvr5/static/media/wi-day-cloudy.3237f7da.svg"
+  },
+  {
+    "revision": "ac93e407f620ec5eee663939681d518f",
+    "url": "/ptgvr5/static/media/wi-day-rain.ac93e407.svg"
+  },
+  {
+    "revision": "86f986fca5306e37fe323f0b8b7cc389",
+    "url": "/ptgvr5/static/media/wi-day-showers.86f986fc.svg"
+  },
+  {
+    "revision": "0437c5fe50f97b1f696b135b8736f58a",
+    "url": "/ptgvr5/static/media/wi-day-snow.0437c5fe.svg"
+  },
+  {
+    "revision": "4b9869a6859846e524d1985de86894f7",
+    "url": "/ptgvr5/static/media/wi-day-sunny.4b9869a6.svg"
+  },
+  {
+    "revision": "c356784f45aaa40ef57ed9031101d912",
+    "url": "/ptgvr5/static/media/wi-fog.c356784f.svg"
+  },
+  {
+    "revision": "fd90aeab153084786ebff365b91f3419",
+    "url": "/ptgvr5/static/media/wi-na.fd90aeab.svg"
+  },
+  {
+    "revision": "7c36f2b5180391599389cc38849eb89d",
+    "url": "/ptgvr5/static/media/wi-night-alt-cloudy.7c36f2b5.svg"
+  },
+  {
+    "revision": "bc016c3b72a5843f21623b98e9c90f3d",
+    "url": "/ptgvr5/static/media/wi-night-alt-rain.bc016c3b.svg"
+  },
+  {
+    "revision": "8ea1ac4e4686afcd6d32776a8c6879e7",
+    "url": "/ptgvr5/static/media/wi-night-alt-showers.8ea1ac4e.svg"
+  },
+  {
+    "revision": "db72088b07bc32e7beee32cc8408f26f",
+    "url": "/ptgvr5/static/media/wi-night-alt-snow.db72088b.svg"
+  },
+  {
+    "revision": "f86c33c4ccc36dbeb826cbdb5f50594b",
+    "url": "/ptgvr5/static/media/wi-night-clear.f86c33c4.svg"
+  },
+  {
+    "revision": "11f9daa177005434b7579ac59fe246f4",
+    "url": "/ptgvr5/static/media/wi-sunrise.11f9daa1.svg"
+  },
+  {
+    "revision": "d96e94d55441191c134f7bb19690785c",
+    "url": "/ptgvr5/static/media/wi-sunset.d96e94d5.svg"
+  },
+  {
+    "revision": "22fff390b0fcd9c46390fe913b23895d",
+    "url": "/ptgvr5/static/media/wi-thunderstorm.22fff390.svg"
+  }
+]);
